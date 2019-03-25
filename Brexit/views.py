47b824312a933e08d2ce08a27edaf39a9293abb4
Pieter Volders <pieter@mbp-van-pieter.alpaca.int>
2019-03-25 16:44:00 +0100

    werkend

--- Brexit/views.py
+++ Brexit/views.py
@@ -1,20 +1,14 @@
 from django.http import HttpResponse, HttpResponseRedirect
 import datetime
-# 29 March 2019 at 11 pm UK time.
+from django.shortcuts import render
+
+# 29 March 2019 at 11 pm UK time. + 1h to compensate UK time
 # Create your views here.
 def index(request):
-    td = datetime.datetime(2019, 3, 29,23,0,0) - datetime.datetime.now()
-    time = td.total_seconds()
-    # hours = seconds // 3600
-    # minutes = (seconds % 3600) // 60
-    # seconds = seconds % 60
 
-    day = time // (24 * 3600)
-    time = time % (24 * 3600)
-    hour = time // 3600
-    time %= 3600
-    minutes = time // 60
-    time %= 60
-    seconds = time
+    Today = datetime.datetime.now()
+    brexit = datetime.datetime(2019, 3, 29,23,0,0)
 
-    return HttpResponse("REMAINING: {0} days - {1}h {2}m {3}s".format(day, hour,minutes,seconds))
+    remaining = brexit - Today
+
+    return HttpResponse("Time remaining untill BREXIT: {0}ms".format(remaining))
